63599aa8177d3d54f05b6ee8d25999d63ddda366
nirav <niravnikhil@gmail.com>
2025-03-02 21:32:49 +0545

    Make releasecard global

--- src/theme/MDXComponents/index.js
+++ src/theme/MDXComponents/index.js
@@ -2,23 +2,21 @@
 import MDXComponents from "../MDXComponentsOriginal/index";
 import Tabs from "@theme/Tabs";
 import TabItem from "@theme/TabItem";
+import DocCard from "@theme/DocCard";
+import DocCardList from "@theme/DocCardList";
 
+// Custom components
 import { Language, LangItem } from "../../components/Language/Language";
 import LangSwitch from "../../components/Language/LangSwitch";
 import { AlphaBadge, BetaBadge } from "../../components/AlphaBetaBadges";
-import DocCardList from "@theme/DocCardList";
 import { APITable, APITableRow } from "../../components/APITable";
-import DocCard from "@theme/DocCard";
 import { UseCaseView, UseCaseLinks } from "../../components/HomepageFeatures";
 import Steps from "../../components/Steps";
 import Subtitle from "../../components/typography/Subtitle";
-
-// Custom components
 import { Accordion, AccordionGroup } from "../../components/Extras/Accordion";
 import { Card, CardGroup } from "../../components/Extras/Card";
-import CodeGroup from "../../components/Extras/Code";
 import Frame from "../../components/Extras/Frame/Frame";
-import Slideshow from "../../components/Extras/Slideshow/Slideshow";
+import ReleaseCard from "../../components/Blog/ReleaseCard";
 
 export default {
   ...MDXComponents,
@@ -51,4 +49,6 @@ export default {
   Subtitle,
   UseCaseLinks,
   UseCaseView,
+
+  ReleaseCard,
 };
