d7571cdb4273faf7e76fc7dd67b91e42855fdafa
nirav <niravnikhil@gmail.com>
2025-03-04 01:56:46 +0545

    add forced mode

--- src/components/Blog/ReleaseCard/index.tsx
+++ src/components/Blog/ReleaseCard/index.tsx
@@ -1,70 +1,33 @@
-import React, { useTransition } from "react";
+import React from "react";
 import { useColorMode } from "@docusaurus/theme-common";
-import { mapLibraryToStyle, ReleaseCardStyle } from "./imagesConfig";
+import { mapLibraryToStyle } from "./imagesConfig";
+import { ReleaseImage } from "./ReleaseImage";
 
 interface ReleaseCardProps {
   library: string;
   version: string;
   releaseType: "major" | "minor" | "patch";
+  forceMode?: "none" | "light" | "dark";
 }
 
-const getImageWithText = (
-  library: string,
-  version: string,
-  releaseType: "major" | "minor" | "patch",
-  style: ReleaseCardStyle,
-) => {
-  const canvas = document.createElement("canvas");
-  const img = new Image();
-  img.src = style[releaseType];
-
-  return new Promise<string>((resolve) => {
-    img.onload = () => {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      const ctx = canvas.getContext("2d");
-
-      if (!ctx) {
-        throw new Error("Could not get canvas context");
-      }
-
-      ctx.drawImage(img, 0, 0);
-
-      ctx.fillStyle = style.font.fillStyle;
-      ctx.font = style.font.style();
-      ctx.textAlign = "center";
-
-      const text = `${library.trim()} ${version.trim()}`.trim();
-      const { x, y } = style.font.position(canvas.width, canvas.height);
-      ctx.fillText(text, x, y);
-
-      resolve(canvas.toDataURL());
-    };
-  });
-};
-
-export default function ReleaseCard({ library, version, releaseType }: ReleaseCardProps) {
+export default function ReleaseCard({
+  library,
+  version,
+  releaseType,
+  forceMode = "dark",
+}: ReleaseCardProps) {
   const { colorMode } = useColorMode();
-  const [combinedImage, setCombinedImage] = React.useState<string>("");
-  const [isPending, startTransition] = useTransition();
 
-  React.useEffect(() => {
-    const style = mapLibraryToStyle(library, colorMode);
-
-    startTransition(() => {
-      getImageWithText(library, version, releaseType, style).then((image) =>
-        setCombinedImage(image),
-      );
-    });
-  }, [version, library, releaseType, colorMode]);
+  const style = mapLibraryToStyle(library, forceMode === "none" ? colorMode : forceMode);
 
   return (
     <div style={{ aspectRatio: "2" }}>
-      {isPending ? (
-        <div style={{ aspectRatio: "2", background: "#f0f0f0" }} />
-      ) : (
-        <img src={combinedImage} alt={`${library} ${version} release`} />
-      )}
+      <ReleaseImage
+        library={library}
+        version={version}
+        releaseType={releaseType}
+        style={style}
+      />
     </div>
   );
 }
