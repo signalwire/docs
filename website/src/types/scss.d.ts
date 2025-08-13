9b70bd38ddd29c26d0211d6e1b3c233b06ae6a0b
hey-august <alindgrenruby@gmail.com>
2025-08-13 12:08:18 -0400

    don't need types

--- website/src/types/scss.d.ts
+++ website/src/types/scss.d.ts
@@ -2,24 +2,3 @@ declare module '*.scss' {
   const content: { [className: string]: string };
   export default content;
 }
-
-// Noticeable SDK global types
-declare global {
-  interface Window {
-    noticeable: {
-      render: (type: string, id: string, options?: any) => void;
-      on: (event: string, callback: Function) => void;
-      off: (event: string, callback: Function) => void;
-      identify: (userId: string, userProperties?: any) => void;
-      reset: () => void;
-      destroy: () => void;
-      ready: (callback: Function) => void;
-      set: (properties: any) => void;
-      do: (action: string, properties?: any) => void;
-      is: (property: string) => any;
-      safe: (callback: Function) => void;
-      help: () => void;
-      debug: (enabled: boolean) => void;
-    };
-  }
-} 
