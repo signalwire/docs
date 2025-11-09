0f56efe008c724831ada61d90f1ccc261e9e9cba
Devon White <devonwhiteit@gmail.com>
2025-11-09 10:27:29 -0500

    Update mobile secondary navbar back button label

--- website/src/theme/Navbar/MobileSidebar/SecondaryMenu/index.tsx
+++ website/src/theme/Navbar/MobileSidebar/SecondaryMenu/index.tsx
@@ -2,15 +2,22 @@ import React, {type ComponentProps, type ReactNode} from 'react';
 import {useThemeConfig} from '@docusaurus/theme-common';
 import {useNavbarSecondaryMenu} from '@docusaurus/theme-common/internal';
 import Translate from '@docusaurus/Translate';
+import {useSecondaryNavState} from '@theme/Navbar/hooks/useSecondaryNavState';
 
-function SecondaryMenuBackButton(props: ComponentProps<'button'>) {
+function SecondaryMenuBackButton(props: ComponentProps<'button'> & {productTitle?: string}) {
+  const {productTitle, ...buttonProps} = props;
+
   return (
-    <button {...props} type="button" className="clean-btn navbar-sidebar__back">
-      <Translate
-        id="theme.navbar.mobileSidebarSecondaryMenu.backButtonLabel"
-        description="The label of the back button to return to main menu, inside the mobile navbar sidebar secondary menu (notably used to display the docs sidebar)">
-        ← Back to main menu
-      </Translate>
+    <button {...buttonProps} type="button" className="clean-btn navbar-sidebar__back">
+      {productTitle ? (
+        `← Back to ${productTitle} Navigation`
+      ) : (
+        <Translate
+          id="theme.navbar.mobileSidebarSecondaryMenu.backButtonLabel"
+          description="The label of the back button to return to main menu, inside the mobile navbar sidebar secondary menu (notably used to display the docs sidebar)">
+          ← Back to main menu
+        </Translate>
+      )}
     </button>
   );
 }
@@ -20,11 +27,16 @@ function SecondaryMenuBackButton(props: ComponentProps<'button'>) {
 export default function NavbarMobileSidebarSecondaryMenu(): ReactNode {
   const isPrimaryMenuEmpty = useThemeConfig().navbar.items.length === 0;
   const secondaryMenu = useNavbarSecondaryMenu();
+  const {product} = useSecondaryNavState();
+
   return (
     <>
       {/* edge-case: prevent returning to the primaryMenu when it's empty */}
       {!isPrimaryMenuEmpty && (
-        <SecondaryMenuBackButton onClick={() => secondaryMenu.hide()} />
+        <SecondaryMenuBackButton
+          onClick={() => secondaryMenu.hide()}
+          productTitle={product?.title}
+        />
       )}
       {secondaryMenu.content}
     </>
