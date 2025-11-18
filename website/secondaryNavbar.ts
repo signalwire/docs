2e8ec63c4b26f7473e56f9293842ac0ad14011cb
august <august.lindgrenruby@signalwire.com>
2025-11-18 15:00:41 -0500

    make `title` optional for main interface

--- website/secondaryNavbar.ts
+++ website/secondaryNavbar.ts
@@ -62,7 +62,7 @@ export interface ProductItem {
 
 export interface ModalSection {
   type: "main" | "section";
-  title: string;
+  title?: string;
   items: Record<string, ProductItem>;
   position?: number;
 }
