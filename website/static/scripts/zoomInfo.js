15b2e3200b8293396c24705dd467ff3eee419efc
Devon <86693904+Devon-White@users.noreply.github.com>
2025-11-19 17:03:27 -0500

    add zoomInfo & remove Koala and Apollo scripts (#647)

---
+++ website/static/scripts/zoomInfo.js
@@ -0,0 +1,11 @@
+function initZoomInfo(){
+  window['zaraz.consent']='e86bc745611694468564';
+  var zi=document.createElement('script');
+  zi.type='text/javascript',
+  zi.async=!0,
+  zi.src='https://www.clarity.ms/tag/fts6rbnz',
+  document.readyState==='complete'?document.body.appendChild(zi):window.addEventListener('load',function(){
+    document.body.appendChild(zi)
+  })
+}
+initZoomInfo();
